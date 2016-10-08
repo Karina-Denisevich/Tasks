3556c7aa3213d3c2904f71073092948a5b9a20a9
Karina Denisevich <karina_41997@mail.ru>
2016-10-09 00:55:06 +0300

    master: generic parameter added

--- src/main/java/com/github/Karina_Denisevich/task3_4/utils/IOUtil.java
+++ src/main/java/com/github/Karina_Denisevich/task3_4/utils/IOUtil.java
@@ -1,12 +1,10 @@
 package com.github.Karina_Denisevich.task3_4.utils;
 
-import com.github.Karina_Denisevich.task3_4.stationery.Stationery;
-
 import java.util.List;
 
 public class IOUtil {
 
-    public void writeList(String message, List<Stationery> stationeryList) {
+    public void writeList(String message, List<?> stationeryList) {
         System.out.println(message);
         stationeryList.forEach(System.out::println);
     }
